e45b2fa7d1ac2e3e049a48fb29b7af58ae438a28
André "MG" Wisén <andre.wisen@gmail.com>
2020-11-25 12:35:46 +0100

    Add types

--- src/utils/ifc-definitions.ts
+++ src/utils/ifc-definitions.ts
@@ -7,14 +7,14 @@ class IfcDefinitions {
      * The property set is a container class that holds {@link IfcPropertySingleValue | properties} within a property tree.
      * These properties are interpreted according to their name attribute.
      */
-    IfcPropertySet = undefined;
+    IfcPropertySet: any = undefined;
 
     /**
      * __IFC2x3 Definition__:
      * A property with a single value (_IfcPropertySingleValue_) defines a property object which has a single (numeric or descriptive) value assigned.
      * It defines a property - single value combination for which the property name, the value with measure type (and optionally the unit) is given.
      */
-    IfcPropertySingleValue = undefined;
+    IfcPropertySingleValue: any = undefined;
 
     /**
      * __IFC2x3 Definition__:
@@ -24,14 +24,14 @@ class IfcDefinitions {
      * The _IfcRelDefinesByProperties_ is a 1-to-N relationship, as it allows for the assignment of one {@link IfcPropertySet | property sets} to a single or to many objects.
      * Those objects then share the same property definition.
      */
-    IfcRelDefinesByProperties = undefined;
+    IfcRelDefinesByProperties: any = undefined;
 
     /**
      * __IFC2x3 Definition__:
      * Major functional part of a building, examples are foundation, floor, roof, wall.
      * The building element comprises all elements that are primarily part of the construction of a building, i.e., its structural and space separating system.
      */
-    IfcBuildingElement = undefined;
+    IfcBuildingElement: any = undefined;
 }
 
 export { IfcDefinitions };
