c3195ec83cec9507d526c756f2601b874e4dda4a
André "MG" Wisén <andre.wisen@gmail.com>
2020-11-25 07:21:51 +0100

    Add example config file

---
+++ src/config/example.config.ts
@@ -0,0 +1,47 @@
+// Config file for Deno
+
+/**
+ * ## Required IFC Entities
+ * These entities are required to parse {@link PROPERTYSET | Property Sets}.
+ */
+const requiredEntities: string[] = [
+    "IFCPROPERTYSINGLEVALUE",
+    "IFCRELDEFINESBYPROPERTIES",
+    "IFCPROPERTYSET",
+];
+/**
+ * ## Selected IFC Entities
+ * These are the selected entities that will be parsed.
+ */
+const selectedEntities: string[] = ["IFCDOOR", "IFCWALLSTANDARDCASE"];
+
+/**
+ * ## All IFC Entities
+ * These are all IFC Entities that will be parsed.
+ * All other entities will be ignored.
+ *
+ */
+const allEntities: string[] = [...requiredEntities, ...selectedEntities];
+
+/**
+ * ## File Path
+ * Generic file path for Deno, relative to `index.ts`
+ *
+ * N.B. Use a [FileReader](https://developer.mozilla.org/en-US/docs/Web/API/FileReader) when implementing onto a website
+ *
+ */
+const filePath: string = "./SimpleWall_Revit2021_CUSTOM.ifc";
+
+/**
+ * ## Options
+ * Export configurations as generic options object.
+ * This need to be better implemented
+ */
+const config: any = {
+    requiredEntities: requiredEntities,
+    selectedEntities: selectedEntities,
+    allEntities: allEntities,
+    filePath: filePath,
+};
+
+export { config };
