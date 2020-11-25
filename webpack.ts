088a91ba9c9fa00b60ab5a73f40b61271c69ae3c
André "MG" Wisén <andre.wisen@gmail.com>
2020-11-25 12:39:53 +0100

    Add webpack

---
+++ webpack.ts
@@ -0,0 +1,31 @@
+import { config } from "./src/config/config.ts";
+import { IfcFile } from "./src/ifc-parser/ifc-parser.ts";
+
+function printPropertySet(ifcFile: IfcFile, propertySet: string) {
+    for (var entitiy in ifcFile.entities.genericEntities) {
+        for (var pset in ifcFile.entities.genericEntities[entitiy].properties) {
+            if (pset === propertySet) {
+                console.log(
+                    ifcFile.entities.genericEntities[entitiy].properties[pset]
+                );
+            }
+        }
+    }
+}
+
+function handleFile(event: Event) {
+    const file = (<HTMLInputElement>event!.target).files![0];
+    var fileReader = new FileReader();
+    fileReader.onload = (loadEvent: Event) => {
+        const lines: string[] = (<string>fileReader!.result).split(/\r\n|\n/);
+        let ifcFile = new IfcFile(lines, config);
+        ifcFile.parseIfcFile();
+        printPropertySet(ifcFile, "Custom_Pset");
+    };
+    fileReader.readAsText(file);
+}
+
+const inputElement = document.getElementById("ifcFileInput");
+inputElement!.addEventListener("change", (event: Event) => {
+    handleFile(event);
+});
